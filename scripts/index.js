d58d0c27c517ae8b2f114a4895b8fb12e7795a01
Marcos Visentini <marcosvisentini@gmail.com>
2024-04-04 19:31:23 -0300

    rp7BHUQR8U1hQapGUjPWl7CbEF2HobRM

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,35 @@
-$(document).ready(() => {
+const generateRandomInteger = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const digestMessage = async (message) => {
+  const messageBuffer = new TextEncoder().encode(message);                    
+  const hashBuffer = await crypto.subtle.digest('SHA-512', messageBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+};
+
+const generateBackgroundColor = async () => {
+  const hash = await digestMessage(luxon.DateTime.now().toLocaleString());
+  return `#${hash.substring(0, 6).toUpperCase()}`;
+};
+
+$(document).ready(async () => {
+  luxon.Settings.defaultLocale = 'pt-BR';
+  luxon.Settings.defaultZone = 'America/Sao_Paulo';
+
+  const currentDayAndMonth = luxon.DateTime.now().toFormat('dd/MM');
+
+  let backgroundColor = '#660000';
+
+  if (['05/03', '09/08', '08/09', '16/12'].includes(currentDayAndMonth)) {
+    backgroundColor = await generateBackgroundColor();
+  }
+  
+  $('body').css('background-color', backgroundColor);
+  
   const emojiTransitions = {
     'deaf-man-emoji': 'man-bowing-emoji',
     'man-bowing-emoji': 'man-shrugging-emoji',

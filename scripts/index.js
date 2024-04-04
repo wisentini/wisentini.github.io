a9446c9bf06a9c554059c4fed7fcfb5bc33b0d7a
Marcos Visentini <marcosvisentini@gmail.com>
2024-04-04 20:23:54 -0300

    072HC4UXoyldMrU1FlMbv3ziIsy7eN8E

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,30 @@
+luxon.Settings.defaultLocale = 'pt-BR';
+luxon.Settings.defaultZone = 'America/Sao_Paulo';
+
 const digestMessage = async (message) => {
-  const messageBuffer = new TextEncoder().encode(message);                    
+  const messageBuffer = new TextEncoder().encode(message);
+
   const hashBuffer = await crypto.subtle.digest('SHA-512', messageBuffer);
+
   const hashArray = Array.from(new Uint8Array(hashBuffer));
+
   return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
 };
 
 const generateBackgroundColor = async () => {
+  const currentDayAndMonth = luxon.DateTime.now().toFormat('dd/MM');
+
+  if (!['05/03', '09/08', '08/09', '16/12', '04/04'].includes(currentDayAndMonth)) {
+    return '#660000';
+  }
+
   const hash = await digestMessage(luxon.DateTime.now().toLocaleString());
+
   return `#${hash.substring(0, 6).toUpperCase()}`;
 };
 
 $(document).ready(async () => {
-  luxon.Settings.defaultLocale = 'pt-BR';
-  luxon.Settings.defaultZone = 'America/Sao_Paulo';
-
-  const currentDayAndMonth = luxon.DateTime.now().toFormat('dd/MM');
-
-  let backgroundColor = '#660000';
-
-  if (['05/03', '09/08', '08/09', '16/12'].includes(currentDayAndMonth)) {
-    backgroundColor = await generateBackgroundColor();
-  }
+  const backgroundColor = await generateBackgroundColor();
   
   $('body').css('background-color', backgroundColor);
   
